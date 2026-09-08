809497aef22931ad97d71f351be06ad3ad02bbfb
nmureire85 <nmureire@gmail.com>
2026-09-08 13:56:39 +0100

    Fixes #2 , closes #3 , resolves #4 ,

--- AIActions.cs
+++ AIActions.cs
@@ -2,7 +2,6 @@ namespace TicTacToe;
 
 public class AIActions
 {
-    
     public static void AIMakeMove(char[,] grid, char symbol, int aiRow, int aiColumn)
     {
         grid[aiRow, aiColumn] = symbol;
